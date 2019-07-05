df7a6b68af5170554803b81daaaaf6274ca0e1f0
Fynn Becker <post@fynn.be>
2019-07-05 14:06:02 +0200

    chore: Replace Express with simple Node http server

--- commands/dev.js
+++ commands/dev.js
@@ -1,10 +1,11 @@
 const chalk = require('chalk')
 const chokidar = require('chokidar')
 const consola = require('consola')
-const express = require('express')
+const fs = require('fs')
 const generateWebSocketScript = require('../lib/generate-weboscket-script')
 const getData = require('../lib/get-data')
 const getPort = require('../lib/get-port')
+const http = require('http')
 const injectScript = require('../lib/inject-script')
 const renderMJML = require('../lib/render-mjml')
 const renderNunjucks = require('../lib/render-nunjucks')
@@ -37,19 +38,35 @@ async function dev (options) {
   const serverPort = await getPort(options.port || 3000)
   const socketPort = await getPort(serverPort + 1)
 
-  const server = express()
   const socket = new WebSocket.Server({ port: socketPort })
 
   const socketScript = generateWebSocketScript({ port: socketPort })
 
-  server.use(express.static('src/attachments'))
+  const requestHandler = (request, response) => {
+    // Handle file requests
+    if (request.url !== '/') {
+      const file = `${process.cwd()}/src/attachments${request.url}`
 
-  server.get('/', (request, response) => {
+      try {
+        const data = fs.readFileSync(file)
+        response.end(data)
+      } catch (error) {
+        if (error) {
+          response.statusCode = 404
+          response.end()
+        }
+      }
+
+      return
+    }
+
+    // Handle HTML request
     const mjmlOutput = renderMJML({ path: options.templatePath })
 
     if (mjmlOutput.errors.length) {
       consola.error(mjmlOutput.errors)
-      response.status(500).end()
+      response.statusCode = 500
+      response.end()
       return
     }
 
@@ -59,7 +76,7 @@ async function dev (options) {
     })
 
     if (!options.data) {
-      return response.send(injectOutput)
+      return response.end(injectOutput)
     }
 
     const data = getData({ data: options.data })
@@ -68,12 +85,18 @@ async function dev (options) {
       context: data
     })
 
-    response.send(nunjucksOutput)
+    response.end(nunjucksOutput)
+  }
+
+  const server = http.createServer(requestHandler)
+
+  server.listen(serverPort, error => {
+    if (error) {
+      return consola.error(error)
+    }
+
+    consola.info(chalk`Development server running at {blue http://localhost:${serverPort}}`)
   })
-
-  server.listen(serverPort)
-
-  consola.info(chalk`Development server running at {blue http://localhost:${serverPort}}`)
 
   chokidar
     .watch(['src/**/*.mjml', 'data/*.json'], { ignoreInitial: true })
